ff8919ff21f40f0e2f67aa09dc006fb149f44681
ds931836 <daniel.svens@telenor.se>
2018-12-03 20:48:46 +0100

    part 1, part 2

---
+++ day_01/day_01.py
@@ -0,0 +1,31 @@
+from itertools import cycle
+
+# Part One
+frequency = 0
+
+with open('input_data.txt', 'r') as f:
+    for n in f:
+        frequency += int(n)
+
+print('Part one answer: {}'.format(frequency))
+
+
+# Part Two
+def run():
+
+    freq = 0
+    found = []
+    input_data = cycle(open('input_data.txt'))
+
+    while True:
+
+        for num in input_data:
+
+            found.append(freq)
+            freq += int(num)
+
+            if freq in found:
+                return freq
+
+
+print('Part two answer: {}'.format(run()))

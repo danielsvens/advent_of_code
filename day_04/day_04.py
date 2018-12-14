6539dceed8adcfed9b71cfc7f5de52e4b95e69f9
ds931836 <daniel.svens@telenor.se>
2018-12-14 09:06:17 +0100

    day 4 almost done

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -1,50 +1,62 @@
-from pprint import pprint
 from datetime import datetime
 
-data_list = list()
-guards = {}
 
-guard_id = 0
-start_time = ''
+def get_data():
 
-with open('input_data.txt', 'r') as f:
-    for line in f.readlines():
-        data_list.append(line.strip())
+    data_list = list()
 
-for guard in data_list:
-    if guard.split()[2] == 'Guard':
-        guards.update({int(guard.split()[3].replace('#', '')): {'start_time': [],
-                                                                'shift': []}})
+    with open('test_data.txt', 'r') as f:
+        for line in f.readlines():
+            data_list.append(line.strip())
 
-data_list.sort()
-
-for guard in data_list:
-    if guard.split()[2] == 'Guard':
-        guard_id = int(guard.split()[3].replace('#', ''))
-        #start_time = '{} {}'.format(guard.split()[0], guard.split()[1])
-    else:
-        #guards[guard_id]['start_time'].append(start_time)
-        guards[guard_id]['shift'].append('{} {}'.format(guard.split()[0].replace('[', ''),
-                                                              guard.split()[1].replace(']', '')))
-
-minutes = dict()
-
-for time in guards.items():
-    one = [datetime.strptime(x, '%Y-%m-%d %H:%M') for x in time[1]['shift'][::2]]
-    two = [datetime.strptime(x, '%Y-%m-%d %H:%M') for x in time[1]['shift'][1::2]]
-    result = [(x.total_seconds() / 60) - 1 for x in [b - a for a, b in zip(one, two)]]
-    try:
-        minutes[time[0]] = max(result) * time[0]
-    except ValueError:
-        pass
-
-print(max([int(id) for id in minutes.values()]))
+    return data_list
 
 
+def part_one(data_list):
+
+    guards = dict()
+    guard_id = 0
+    #start_time = ''
+
+    for guard in data_list:
+        if guard.split()[2] == 'Guard':
+            guards.update({int(guard.split()[3].replace('#', '')): {'start_time': [],
+                                                                    'shift': []}})
+
+    data_list.sort()
+
+    for guard in data_list:
+        if guard.split()[2] == 'Guard':
+            guard_id = int(guard.split()[3].replace('#', ''))
+            #start_time = '{} {}'.format(guard.split()[0], guard.split()[1])
+        else:
+            #guards[guard_id]['start_time'].append(start_time)
+            guards[guard_id]['shift'].append('{} {}'.format(guard.split()[0].replace('[', ''),
+                                                                  guard.split()[1].replace(']', '')))
+
+    minutes = dict()
+
+    for time in guards.items():
+        one = [datetime.strptime(x, '%Y-%m-%d %H:%M') for x in time[1]['shift'][::2]]
+        two = [datetime.strptime(x, '%Y-%m-%d %H:%M') for x in time[1]['shift'][1::2]]
+
+        print(len(one), len(two))
+        result = [(x.total_seconds() / 60 - 1) for x in [b - a for a, b in zip(one, two)] if x.total_seconds() / 60 != 1]
+        try:
+            minutes[time[0]] = max(result)
+        except ValueError:
+            pass
+
+    print(minutes)
+    guard_id = [k * v for k, v in minutes.items() if v == max([int(id) for id in minutes.values()])]
+
+    return guard_id
 
 
+if __name__ == '__main__':
 
+    data = get_data()
+    part_one_result = part_one(data)
 
+    print(part_one_result)
 
-
-#pprint(guards.items())
